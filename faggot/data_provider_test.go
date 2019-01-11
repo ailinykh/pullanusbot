fef1aceb392b16f4e099b9b2ac7f914b33943fe7
Anthony Ilinykh <anthonyilinykh@gmail.com>
2019-01-11 22:33:44 +0300

    data provider tests refactoring

--- faggot/data_provider_test.go
+++ faggot/data_provider_test.go
@@ -2,12 +2,70 @@ package faggot
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"testing"
-	"time"
 )
 
+func TestDataProviderSavesJson(t *testing.T) {
+	workingDir := path.Join(os.TempDir(), fmt.Sprintf("faggot_bot_data_%s", randStringRunes(4)))
+	defer os.RemoveAll(workingDir)
+	t.Logf("Using data directory: %s", workingDir)
+
+	filename := "test_file.json"
+	file := path.Join(workingDir, filename)
+	dp := NewDataProvider(workingDir)
+	dataMock := []byte(`{
+			"players": [],
+			"entries": []
+		}`)
+
+	dp.saveJSON(filename, dataMock)
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		t.Errorf("File not created! %s", file)
+	}
+
+	data, _ := ioutil.ReadFile(file)
+
+	if string(data) != string(dataMock) {
+		t.Log(data)
+		t.Error("File has wrong content!")
+	}
+}
+
+func TestDataProviderLoadsJson(t *testing.T) {
+	workingDir := path.Join(os.TempDir(), fmt.Sprintf("faggot_bot_data_%s", randStringRunes(4)))
+	defer os.RemoveAll(workingDir)
+	t.Logf("Using data directory: %s", workingDir)
+
+	filename := "test_file.json"
+	file := path.Join(workingDir, filename)
+	dp := NewDataProvider(workingDir)
+	dataMock := []byte(`{
+			"players": [],
+			"entries": []
+		}`)
+	ioutil.WriteFile(file, dataMock, 0644)
+
+	data := dp.loadJSON(filename)
+
+	if string(data) != string(dataMock) {
+		t.Log(data)
+		t.Error("File has wrong content!")
+	}
+
+	// test dp creates file on load if not exist
+	notExistingFilename := "not_existing_file.json"
+	dp.loadJSON(notExistingFilename)
+
+	if _, err := os.Stat(path.Join(workingDir, notExistingFilename)); os.IsNotExist(err) {
+		t.Error("Data provider must create file if not exist!")
+	}
+}
+
+/*
 func TestDataProvider(t *testing.T) {
 	workingDir := path.Join(os.TempDir(), fmt.Sprintf("faggot_bot_data_%s", randStringRunes(4)))
 	defer func() {
@@ -56,3 +114,4 @@ func TestDataProvider(t *testing.T) {
 		t.Errorf("Not all files created (%d)", len(arr))
 	}
 }
+*/
