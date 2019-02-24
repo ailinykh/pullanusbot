77f949c69a8d59f69567403e98df497e9d69d834
Anthony Ilinykh <anthonyilinykh@gmail.com>
2019-02-24 22:06:51 +0300

    bitrate and mutex added

--- converter.go
+++ converter.go
@@ -6,12 +6,15 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
+	"sync"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 // Converter helps to post video files proper way
 type Converter struct {
+	mutex sync.Mutex
 }
 
 func (c *Converter) initialize() {
@@ -21,6 +24,10 @@ func (c *Converter) initialize() {
 
 func (c *Converter) checkMessage(m *tb.Message) {
 	if m.Document.MIME[:5] == "video" {
+		// Just one video at one time pls
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
+
 		log.Printf("Converter: Got video! \"%s\" of type %s from %s", m.Document.FileName, m.Document.MIME, m.Sender.Username)
 
 		if m.Document.FileSize > 20*1024*1024 {
@@ -45,8 +52,18 @@ func (c *Converter) checkMessage(m *tb.Message) {
 
 		log.Println("Converter: Video downloaded. Converting...")
 
-		cmd := exec.Command("/bin/sh", "-c", "ffmpeg -y -i \""+sourceFile+"\" -c:v libx264 -preset medium -b:v 555k -pass 1 -b:a 128k -f mp4 /dev/null && ffmpeg -y -i \""+sourceFile+"\" -c:v libx264 -preset medium -b:v 555k -pass 2 -b:a 128k \""+destinationFile+"\"")
-		err := cmd.Run()
+		bitrate, err := c.getBitrate(sourceFile)
+		if err != nil {
+			log.Printf("Converter: Bitrate converting error: %s", err)
+			return
+		}
+		bitrate /= 1024
+		if bitrate > 600 {
+			bitrate = 555
+		}
+
+		cmd := exec.Command("/bin/sh", "-c", "ffmpeg -y -i \""+sourceFile+"\" -c:v libx264 -preset medium -b:v "+strconv.Itoa(bitrate)+"k -pass 1 -b:a 128k -f mp4 /dev/null && ffmpeg -y -i \""+sourceFile+"\" -c:v libx264 -preset medium -b:v "+strconv.Itoa(bitrate)+"k -pass 2 -b:a 128k \""+destinationFile+"\"")
+		err = cmd.Run()
 		if err != nil {
 			log.Printf("Converter: Video converting error: %s", err)
 			return
@@ -73,4 +90,20 @@ func (c *Converter) checkMessage(m *tb.Message) {
 	} else {
 		log.Printf("Converter: %s is not mpeg video", m.Document.MIME)
 	}
+}
+
+func (c *Converter) getBitrate(file string) (int, error) {
+	out, err := exec.Command("/bin/sh", "-c", "ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 \""+file+"\"").Output()
+	if err != nil {
+		log.Printf("Converter: Bitrate retreiving error: %s", err)
+		return 0, err
+	}
+
+	bitrate, err := strconv.Atoi(string(out[:len(out)-1])) // "strip EOL"
+	if err != nil {
+		log.Printf("Converter: Bitrate converting error: %s", err)
+		return 0, err
+	}
+
+	return bitrate, nil
 }
