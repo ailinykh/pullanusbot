45974963d7051776ecddabd1dcad167c3f43f5c4
Anthony Ilinykh <anthonyilinykh@gmail.com>
2020-09-01 23:12:48 +0300

    [{Rate limit exceeded 88}]

--- twitter.go
+++ twitter.go
@@ -8,7 +8,9 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/google/logger"
 	tb "gopkg.in/tucnak/telebot.v2"
@@ -75,155 +77,177 @@ type twitterVideoInfoVariant struct {
 }
 
 func (t *Twitter) handleTextMessage(m *tb.Message) {
+	r, _ := regexp.Compile(`twitter\.com.+/(\d+)\S*$`)
+	match := r.FindStringSubmatch(m.Text)
+	if len(match) > 1 {
+		t.processTweet(m, match[1])
+	}
+}
+
+func (t *Twitter) processTweet(m *tb.Message, tweetID string) {
 	b, ok := bot.(*tb.Bot)
 	if !ok {
 		logger.Errorf("Bot cast failed")
 		return
 	}
 
-	r, _ := regexp.Compile(`twitter\.com.+/(\d+)\S*$`)
-	match := r.FindStringSubmatch(m.Text)
-	if len(match) > 1 {
-		tweetID := match[1]
-		logger.Infof("Found tweet %s", tweetID)
+	logger.Infof("Processing tweet %s", tweetID)
 
-		client := &http.Client{}
-		req, _ := http.NewRequest("GET", fmt.Sprintf("https://api.twitter.com/1.1/statuses/show.json?id=%s&tweet_mode=extended", tweetID), nil)
-		req.Header.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAAPYXBAAAAAAACLXUNDekMxqa8h%2F40K4moUkGsoc%3DTYfbDKbT3jJPCEVnMYqilB28NHfOPqkca3qaAxGfsyKCs0wRbw")
-		res, err := client.Do(req)
-		if err != nil {
-			logger.Errorf("json fetch error: %v", err)
-			return
-		}
-		defer res.Body.Close()
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", fmt.Sprintf("https://api.twitter.com/1.1/statuses/show.json?id=%s&tweet_mode=extended", tweetID), nil)
+	req.Header.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAAPYXBAAAAAAACLXUNDekMxqa8h%2F40K4moUkGsoc%3DTYfbDKbT3jJPCEVnMYqilB28NHfOPqkca3qaAxGfsyKCs0wRbw")
+	res, err := client.Do(req)
+	if err != nil {
+		logger.Errorf("json fetch error: %v", err)
+		return
+	}
+	defer res.Body.Close()
 
-		var twResp twitterReponse
-		body, _ := ioutil.ReadAll(res.Body)
+	var twResp twitterReponse
+	body, _ := ioutil.ReadAll(res.Body)
 
-		err = json.Unmarshal(body, &twResp)
-		if err != nil {
-			logger.Errorf("json parse error: %v", err)
-			return
-		}
+	err = json.Unmarshal(body, &twResp)
+	if err != nil {
+		logger.Errorf("json parse error: %v", err)
+		return
+	}
 
-		if len(twResp.Errors) > 0 {
-			logger.Errorf("twitter api error: %v", twResp.Errors)
-			logger.Errorf("%v", res.Header)
+	if len(twResp.Errors) > 0 {
+		if twResp.Errors[0].Code == 88 { // "Rate limit exceeded 88"
+			limit, err := strconv.ParseInt(res.Header["X-Rate-Limit-Reset"][0], 10, 64)
 
-			b.Send(m.Chat, fmt.Sprint(twResp.Errors), &tb.SendOptions{ReplyTo: m})
-			return
-		}
-
-		caption := t.getCaption(m, twResp)
-		media := twResp.ExtendedEntities.Media
-
-		if len(twResp.ExtendedEntities.Media) == 0 && twResp.QuotedStatus != nil && len(twResp.QuotedStatus.ExtendedEntities.Media) > 0 {
-			caption = t.getCaption(m, *twResp.QuotedStatus)
-			media = twResp.QuotedStatus.ExtendedEntities.Media
-		}
-
-		switch len(media) {
-		case 0:
-			logger.Info("Sending as text")
-
-			for _, url := range twResp.Entities.Urls {
-				caption += "\n" + url.ExpandedURL
-			}
-
-			_, err = b.Send(m.Chat, caption, &tb.SendOptions{ParseMode: tb.ModeHTML, DisableWebPagePreview: true})
-		case 1:
-			if media[0].Type == "video" {
-				file := &tb.Video{File: tb.FromURL(media[0].VideoInfo.best().URL)}
-				file.Caption = caption
-				logger.Infof("Sending as Video %s", file.FileURL)
-				b.Notify(m.Chat, tb.UploadingVideo)
-				_, err = file.Send(b, m.Chat, &tb.SendOptions{ParseMode: tb.ModeHTML})
-			} else if media[0].Type == "photo" {
-				file := &tb.Photo{File: tb.FromURL(media[0].MediaURL)}
-				file.Caption = caption
-				logger.Infof("Sending as Photo %s", file.FileURL)
-				b.Notify(m.Chat, tb.UploadingPhoto)
-				_, err = file.Send(b, m.Chat, &tb.SendOptions{ParseMode: tb.ModeHTML})
-			} else {
-				logger.Infof("Unknown type: %s", media[0].Type)
-				b.Send(m.Chat, fmt.Sprintf("Unknown type: %s", media[0].Type), &tb.SendOptions{ReplyTo: m})
+			if err != nil {
+				logger.Error(err)
 				return
 			}
-		default:
-			logger.Infof("Sending as Album")
-			b.Notify(m.Chat, tb.UploadingPhoto)
-			b.Send(m.Chat, caption, &tb.SendOptions{ParseMode: tb.ModeHTML, DisableWebPagePreview: true})
-			_, err = b.SendAlbum(m.Chat, t.getAlbum(media))
+
+			timeout := limit - time.Now().Unix()
+			logger.Infof("Twitter api timeout %d seconds", timeout)
+
+			go func() {
+				time.Sleep(time.Duration(timeout) * time.Second)
+				t.processTweet(m, tweetID)
+			}()
+
+			return
 		}
 
-		if err == nil {
-			logger.Info("Messages sent. Deleting original")
-			err = b.Delete(m)
-			if err != nil {
-				logger.Errorf("Can't delete original message: %v", err)
-			}
+		logger.Errorf("Twitter api error: %v", twResp.Errors)
+		logger.Errorf("%v", res.Header)
+
+		b.Send(m.Chat, fmt.Sprint(twResp.Errors), &tb.SendOptions{ReplyTo: m})
+		return
+	}
+
+	caption := t.getCaption(m, twResp)
+	media := twResp.ExtendedEntities.Media
+
+	if len(twResp.ExtendedEntities.Media) == 0 && twResp.QuotedStatus != nil && len(twResp.QuotedStatus.ExtendedEntities.Media) > 0 {
+		caption = t.getCaption(m, *twResp.QuotedStatus)
+		media = twResp.QuotedStatus.ExtendedEntities.Media
+	}
+
+	switch len(media) {
+	case 0:
+		logger.Info("Sending as text")
+
+		for _, url := range twResp.Entities.Urls {
+			caption += "\n" + url.ExpandedURL
+		}
+
+		_, err = b.Send(m.Chat, caption, &tb.SendOptions{ParseMode: tb.ModeHTML, DisableWebPagePreview: true})
+	case 1:
+		if media[0].Type == "video" {
+			file := &tb.Video{File: tb.FromURL(media[0].VideoInfo.best().URL)}
+			file.Caption = caption
+			logger.Infof("Sending as Video %s", file.FileURL)
+			b.Notify(m.Chat, tb.UploadingVideo)
+			_, err = file.Send(b, m.Chat, &tb.SendOptions{ParseMode: tb.ModeHTML})
+		} else if media[0].Type == "photo" {
+			file := &tb.Photo{File: tb.FromURL(media[0].MediaURL)}
+			file.Caption = caption
+			logger.Infof("Sending as Photo %s", file.FileURL)
+			b.Notify(m.Chat, tb.UploadingPhoto)
+			_, err = file.Send(b, m.Chat, &tb.SendOptions{ParseMode: tb.ModeHTML})
 		} else {
-			logger.Errorf("Can't send entry: %v", err)
+			logger.Infof("Unknown type: %s", media[0].Type)
+			b.Send(m.Chat, fmt.Sprintf("Unknown type: %s", media[0].Type), &tb.SendOptions{ReplyTo: m})
+			return
+		}
+	default:
+		logger.Infof("Sending as Album")
+		b.Notify(m.Chat, tb.UploadingPhoto)
+		b.Send(m.Chat, caption, &tb.SendOptions{ParseMode: tb.ModeHTML, DisableWebPagePreview: true})
+		_, err = b.SendAlbum(m.Chat, t.getAlbum(media))
+	}
 
-			if strings.HasSuffix(err.Error(), "failed to get HTTP URL content") {
-				// Try to upload file to telegram
-				logger.Info("Sending by uploading")
+	if err == nil {
+		logger.Info("Messages sent. Deleting original")
+		err = b.Delete(m)
+		if err != nil {
+			logger.Errorf("Can't delete original message: %v", err)
+		}
+	} else {
+		logger.Errorf("Can't send entry: %v", err)
 
-				filename := path.Base(media[0].VideoInfo.best().URL)
-				videoFile := path.Join(os.TempDir(), filename)
-				defer os.Remove(videoFile)
+		if strings.HasSuffix(err.Error(), "failed to get HTTP URL content") {
+			// Try to upload file to telegram
+			logger.Info("Sending by uploading")
 
-				err = downloadFile(videoFile, media[0].VideoInfo.best().URL)
+			filename := path.Base(media[0].VideoInfo.best().URL)
+			videoFile := path.Join(os.TempDir(), filename)
+			defer os.Remove(videoFile)
+
+			err = downloadFile(videoFile, media[0].VideoInfo.best().URL)
+			if err != nil {
+				logger.Errorf("video download error: %v", err)
+				return
+			}
+
+			c := Converter{}
+			ffpInfo, err := c.getFFProbeInfo(videoFile)
+			if err != nil {
+				logger.Errorf("FFProbe info retreiving error: %v", err)
+				return
+			}
+
+			videoStreamInfo, err := ffpInfo.getVideoStream()
+			if err != nil {
+				logger.Errorf("%v", err)
+				return
+			}
+
+			video := tb.Video{File: tb.FromDisk(videoFile)}
+			video.Width = videoStreamInfo.Width
+			video.Height = videoStreamInfo.Height
+			video.Duration = ffpInfo.Format.duration()
+			video.SupportsStreaming = true
+			// insert hot link
+			idx := strings.Index(caption, " ")
+
+			video.Caption = caption[0:idx] + fmt.Sprintf(`<a href="%s">🎞</a>`, media[0].VideoInfo.best().URL) + caption[idx:]
+
+			// Getting thumbnail
+			thumb, err := c.getThumbnail(videoFile)
+			if err != nil {
+				logger.Errorf("PlainLink: Thumbnail error: %v", err)
+			} else {
+				video.Thumbnail = &tb.Photo{File: tb.FromDisk(thumb)}
+				defer os.Remove(thumb)
+			}
+
+			logger.Infof("Sending file: w:%d h:%d duration:%d", video.Width, video.Height, video.Duration)
+
+			b.Notify(m.Chat, tb.UploadingVideo)
+			_, err = video.Send(b, m.Chat, &tb.SendOptions{ParseMode: tb.ModeHTML})
+			if err == nil {
+				logger.Info("Video sent. Deleting original")
+				err = b.Delete(m)
 				if err != nil {
-					logger.Errorf("video download error: %v", err)
-					return
+					logger.Errorf("Can't delete original message: %v", err)
 				}
-
-				c := Converter{}
-				ffpInfo, err := c.getFFProbeInfo(videoFile)
-				if err != nil {
-					logger.Errorf("FFProbe info retreiving error: %v", err)
-					return
-				}
-
-				videoStreamInfo, err := ffpInfo.getVideoStream()
-				if err != nil {
-					logger.Errorf("%v", err)
-					return
-				}
-
-				video := tb.Video{File: tb.FromDisk(videoFile)}
-				video.Width = videoStreamInfo.Width
-				video.Height = videoStreamInfo.Height
-				video.Duration = ffpInfo.Format.duration()
-				video.SupportsStreaming = true
-				// insert hot link
-				idx := strings.Index(caption, " ")
-
-				video.Caption = caption[0:idx] + fmt.Sprintf(`<a href="%s">🎞</a>`, media[0].VideoInfo.best().URL) + caption[idx:]
-
-				// Getting thumbnail
-				thumb, err := c.getThumbnail(videoFile)
-				if err != nil {
-					logger.Errorf("PlainLink: Thumbnail error: %v", err)
-				} else {
-					video.Thumbnail = &tb.Photo{File: tb.FromDisk(thumb)}
-					defer os.Remove(thumb)
-				}
-
-				logger.Infof("Sending file: w:%d h:%d duration:%d", video.Width, video.Height, video.Duration)
-
-				b.Notify(m.Chat, tb.UploadingVideo)
-				_, err = video.Send(b, m.Chat, &tb.SendOptions{ParseMode: tb.ModeHTML})
-				if err == nil {
-					logger.Info("Video sent. Deleting original")
-					err = b.Delete(m)
-					if err != nil {
-						logger.Errorf("Can't delete original message: %v", err)
-					}
-				} else {
-					logger.Errorf("Can't send video: %v", err)
-				}
+			} else {
+				logger.Errorf("Can't send video: %v", err)
 			}
 		}
 	}
