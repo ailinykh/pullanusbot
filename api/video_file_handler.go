79bb9f25b367a915e9e1c6cdbec75e45f365dcc4
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-06-24 10:03:16 +0300

    Remove IVdeoFileHandler interface since it is not needed

--- api/video_file_handler.go
+++
@@ -1,8 +0,0 @@
-package api
-
-import "github.com/ailinykh/pullanusbot/v2/core"
-
-// IVdeoFileHandler interface for processing Videos
-type IVdeoFileHandler interface {
-	HandleVideo(*core.Video, core.IBot) error
-}
