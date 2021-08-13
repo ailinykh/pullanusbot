1ee9f7f0848a70da6dbed1861673ee4c7d1224e6
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-08-13 09:52:52 +0300

    Move BotMock into a separate common_test file

--- usecases/faggot_game_test.go
+++ usecases/faggot_game_test.go
@@ -277,24 +277,3 @@ func (s *GameStorageMock) AddRound(gameID int64, round *core.Round) error {
 func (s *GameStorageMock) GetRounds(gameID int64) ([]*core.Round, error) {
 	return s.rounds, nil
 }
-
-type BotMock struct {
-	sentMessages    []string
-	removedMessages []string
-}
-
-func (BotMock) SendImage(*core.Image, string) (*core.Message, error)  { return nil, nil }
-func (BotMock) SendAlbum([]*core.Image) ([]*core.Message, error)      { return nil, nil }
-func (BotMock) SendMedia(*core.Media) (*core.Message, error)          { return nil, nil }
-func (BotMock) SendPhotoAlbum([]*core.Media) ([]*core.Message, error) { return nil, nil }
-func (BotMock) SendVideo(*core.Video, string) (*core.Message, error)  { return nil, nil }
-
-func (b *BotMock) Delete(message *core.Message) error {
-	b.removedMessages = append(b.removedMessages, message.Text)
-	return nil
-}
-
-func (b *BotMock) SendText(text string, args ...interface{}) (*core.Message, error) {
-	b.sentMessages = append(b.sentMessages, text)
-	return nil, nil
-}
