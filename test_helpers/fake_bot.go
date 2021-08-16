617ee871f716f66408d1e748e05c2c3d4dceb2ac
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-08-16 20:52:41 +0300

    Implement SendVideo method for FakeBot

--- test_helpers/fake_bot.go
+++ test_helpers/fake_bot.go
@@ -2,13 +2,16 @@ package test_helpers
 
 import "github.com/ailinykh/pullanusbot/v2/core"
 
+// https://stackoverflow.com/questions/31794141/can-i-create-shared-test-utilities
+
 func CreateFakeBot() *FakeBot {
-	return &FakeBot{[]string{}, []string{}, []string{}}
+	return &FakeBot{[]string{}, []string{}, []string{}, []string{}}
 }
 
 type FakeBot struct {
 	SentMedias      []string
 	SentMessages    []string
+	SentVideos      []string
 	RemovedMessages []string
 }
 
@@ -27,7 +30,10 @@ func (b *FakeBot) SendPhotoAlbum(media []*core.Media) ([]*core.Message, error) {
 	return nil, nil
 }
 
-func (FakeBot) SendVideo(*core.Video, string) (*core.Message, error) { return nil, nil }
+func (b *FakeBot) SendVideo(video *core.Video, caption string) (*core.Message, error) {
+	b.SentVideos = append(b.SentVideos, video.Name)
+	return nil, nil
+}
 
 func (b *FakeBot) Delete(message *core.Message) error {
 	b.RemovedMessages = append(b.RemovedMessages, message.Text)
