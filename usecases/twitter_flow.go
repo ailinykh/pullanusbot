6b1be1d86994f9a810e31ab1d20809641daadc70
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-08-17 11:06:48 +0300

    Delete original message if it completely matches tweet link

--- usecases/twitter_flow.go
+++ usecases/twitter_flow.go
@@ -22,12 +22,22 @@ type TwitterFlow struct {
 
 // HandleTweet is a ITweetHandler protocol implementation
 func (tf *TwitterFlow) HandleTweet(tweetID string, message *core.Message, bot core.IBot, deleteOriginal bool) error {
-	tf.l.Infof("processing tweet %s", tweetID)
+	tf.l.Infof("processing tweet %s, delete original: %t", tweetID, deleteOriginal)
 	media, err := tf.mf.CreateMedia(tweetID, message.Sender)
 	if err != nil {
 		tf.l.Error(err)
 		return err
 	}
 
-	return tf.sms.SendMedia(media, bot)
+	err = tf.sms.SendMedia(media, bot)
+	if err != nil {
+		tf.l.Error(err)
+		return err
+	}
+
+	if deleteOriginal {
+		return bot.Delete(message)
+	}
+
+	return nil
 }
