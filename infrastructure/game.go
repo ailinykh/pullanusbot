4498432594d7c8d6167455842e845212782351b5
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-08-26 09:24:00 +0300

    Move Player and Round infrastructure models into one file

--- infrastructure/game.go
+++ infrastructure/game.go
@@ -14,3 +14,16 @@ type Player struct {
 func (Player) TableName() string {
 	return "faggot_players"
 }
+
+// Round that can be persistent on disk
+type Round struct {
+	GameID   int64
+	UserID   int
+	Day      string `gorm:"primaryKey"`
+	Username string
+}
+
+// TableName gorm API
+func (Round) TableName() string {
+	return "faggot_rounds"
+}
