9fa3f2be65a29233c9234e97b7e8f3a2eb138d0d
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-01-08 21:26:24 +0300

    Migrate to tiktok_api struct in tiktok media factory

--- api/tiktok_media_factory.go
+++ api/tiktok_media_factory.go
@@ -1,26 +1,54 @@
 package api
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/ailinykh/pullanusbot/v2/core"
 )
 
 func CreateTikTokMediaFactory(l core.ILogger, hc core.IHttpClient, r core.IRand) core.IMediaFactory {
-	htmlApi := CreateTikTokHTMLAPI(l, hc, r)
-	jsonApi := CreateTikTokJsonAPI(l, hc, r)
-	return &TikTokMediaFactory{l, htmlApi, jsonApi}
+	return &TikTokMediaFactory{l, &TikTokAPI{l, hc, r}}
 }
 
 type TikTokMediaFactory struct {
-	l       core.ILogger
-	htmlApi core.IMediaFactory
-	jsonApi core.IMediaFactory
+	l   core.ILogger
+	api *TikTokAPI
 }
 
-func (api *TikTokMediaFactory) CreateMedia(url string) ([]*core.Media, error) {
-	media, err := api.jsonApi.CreateMedia(url)
-	if err != nil {
-		return api.htmlApi.CreateMedia(url)
+func (factory *TikTokMediaFactory) CreateMedia(url string) ([]*core.Media, error) {
+	parts := strings.Split(url, "/")
+	if len(parts) < 6 {
+		return nil, fmt.Errorf("unexpected url %s", url)
 	}
 
-	return media, nil
+	item, err := factory.api.getItemUsingJsonApi(parts[3], parts[5])
+	if err != nil {
+		item, err = factory.api.getItemUsingHtmlApi(parts[3], parts[5])
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	title := item.Desc
+	if len(title) == 0 {
+		title = fmt.Sprintf("%s (@%s)", item.Author.Nickname, item.Author.UniqueId)
+	}
+
+	description := fmt.Sprintf("%s (@%s) has created a short video on TikTok with music %s.", item.Author.Nickname, item.Author.UniqueId, item.Music.Title)
+	for _, s := range item.StickersOnItem {
+		for _, t := range s.StickerText {
+			description = description + " | " + t
+		}
+	}
+
+	media := &core.Media{
+		URL:         url,
+		ResourceURL: item.Video.DownloadAddr,
+		Title:       title,
+		Description: description,
+	}
+
+	return []*core.Media{media}, nil
 }
