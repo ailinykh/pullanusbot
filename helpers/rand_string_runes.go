aa23587fa3b3a97ad75fc73663d5ba8d0177f124
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-02-12 23:28:11 +0300

    Add `rand_string_runes.go` to helpers

---
+++ helpers/rand_string_runes.go
@@ -0,0 +1,18 @@
+package helpers
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// RandStringRunes returns a random n-length string
+func RandStringRunes(n int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
