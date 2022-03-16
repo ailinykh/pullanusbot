92ae359ba4a2febbaa231307cc1ba4a4120ae96d
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-03-16 11:17:33 +0300

    Extend default settings with payload array of strings

--- core/settings.go
+++ core/settings.go
@@ -1,12 +1,13 @@
 package core
 
 type Settings struct {
-	FaggotGameCommandsEnabled bool `json:"faggot_game_enabled"`
-	RemoveSourceOnSucccess    bool `json:"remove_source_on_success"`
+	FaggotGameCommandsEnabled bool     `json:"faggot_game_enabled"`
+	Payload                   []string `json:"payload"`
+	RemoveSourceOnSucccess    bool     `json:"remove_source_on_success"`
 }
 
 func DefaultSettings() Settings {
-	return Settings{false, true}
+	return Settings{false, []string{}, true}
 }
 
 type ISettingsStorage interface {
