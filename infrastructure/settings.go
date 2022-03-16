0252247e12d3c50ec110f40c32ea7ac4f3ef0d4b
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-03-16 22:53:28 +0300

    Make ChatID primary key

--- infrastructure/settings.go
+++ infrastructure/settings.go
@@ -3,7 +3,7 @@ package infrastructure
 import "time"
 
 type Settings struct {
-	ChatID    int64
+	ChatID    int64 `gorm:"primaryKey"`
 	Data      []byte
 	CreatedAt time.Time
 	UpdatedAt time.Time `gorm:"autoCreateTime"`
