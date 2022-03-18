c21d452bf1c6753546f4cba66af2932b0f77c775
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-03-18 20:19:19 +0100

    Extend  command with settings upadate

--- usecases/start_flow.go
+++ usecases/start_flow.go
@@ -1,19 +1,31 @@
 package usecases
 
-import "github.com/ailinykh/pullanusbot/v2/core"
+import (
+	"strings"
+
+	"github.com/ailinykh/pullanusbot/v2/core"
+)
 
 func CreateStartFlow(l core.ILogger, loc core.ILocalizer, settingsStorage core.ISettingsStorage) core.ITextHandler {
-	return &StartFlow{l, loc, settingsStorage}
+	return &StartFlow{l, loc, settingsStorage, make(map[int64]bool)}
 }
 
 type StartFlow struct {
 	l               core.ILogger
 	loc             core.ILocalizer
 	settingsStorage core.ISettingsStorage
+	cache           map[int64]bool
 }
 
 // HandleText is a core.ITextHandler protocol implementation
 func (flow *StartFlow) HandleText(message *core.Message, bot core.IBot) error {
+	if strings.HasPrefix(message.Text, "/start") {
+		return flow.handleStart(message, bot)
+	}
+	return flow.handleSettingsChack(message, bot)
+}
+
+func (flow *StartFlow) handleStart(message *core.Message, bot core.IBot) error {
 	settings, err := flow.settingsStorage.GetSettings(message.ChatID)
 	if err != nil {
 		return err
@@ -31,6 +43,18 @@ func (flow *StartFlow) HandleText(message *core.Message, bot core.IBot) error {
 	}
 	_, err = bot.SendText(flow.loc.I18n("start_welcome"))
 	return err
+}
+
+func (flow *StartFlow) handleSettingsChack(message *core.Message, bot core.IBot) error {
+	if _, ok := flow.cache[message.ChatID]; !ok {
+		flow.cache[message.ChatID] = true
+		_, err := flow.settingsStorage.GetSettings(message.ChatID) // create settings if needed
+		if err != nil {
+			flow.l.Error(err)
+			return err
+		}
+	}
+	return nil
 }
 
 func (flow *StartFlow) contains(payload string, current []string) bool {
