8ee41507d3e8eb9085037dea890ca907fcabbf42
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-03-22 15:09:35 +0300

    Make `Chats` variable accessible from outside in test_helpers

--- test_helpers/chat_storage.go
+++ test_helpers/chat_storage.go
@@ -11,13 +11,13 @@ func CreateChatStorage() *FakeChatStorage {
 }
 
 type FakeChatStorage struct {
-	chats map[int64]*core.Chat
+	Chats map[int64]*core.Chat
 	Err   error
 }
 
 // GetChatByID is a core.IChatStorage interface implementation
 func (storage *FakeChatStorage) GetChatByID(chatID int64) (*core.Chat, error) {
-	if user, ok := storage.chats[chatID]; ok {
+	if user, ok := storage.Chats[chatID]; ok {
 		return user, nil
 	}
 	return nil, fmt.Errorf("record not found")
@@ -25,6 +25,7 @@ func (storage *FakeChatStorage) GetChatByID(chatID int64) (*core.Chat, error) {
 
 // CreateChat is a core.IChatStorage interface implementation
 func (s *FakeChatStorage) CreateChat(chatID int64, title string, type_ string, settings *core.Settings) error {
+	s.Chats[chatID] = &core.Chat{ID: chatID, Title: title, Type: type_, Settings: settings}
 	return nil
 }
 
