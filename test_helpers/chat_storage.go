c307f4c1b950c1f1df0f4526d478b8ff59a19ffd
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-03-22 13:38:57 +0300

    Add `FakeChatStorage` implementation of `IChatStorage` for testing

---
+++ test_helpers/chat_storage.go
@@ -0,0 +1,30 @@
+package test_helpers
+
+import (
+	"fmt"
+
+	"github.com/ailinykh/pullanusbot/v2/core"
+)
+
+func CreateChatStorage() *FakeChatStorage {
+	return &FakeChatStorage{make(map[int64]*core.Chat), nil}
+}
+
+type FakeChatStorage struct {
+	chats map[int64]*core.Chat
+	Err   error
+}
+
+// GetChatByID is a core.IChatStorage interface implementation
+func (storage *FakeChatStorage) GetChatByID(chatID int64) (*core.Chat, error) {
+	if user, ok := storage.chats[chatID]; ok {
+		return user, nil
+	}
+	return nil, fmt.Errorf("chat with id %d not found", chatID)
+}
+
+// CreateChat is a core.IChatStorage interface implementation
+func (s *FakeChatStorage) CreateChat(chatID int64, title string, type_ string, settings *core.Settings) error {
+	s.chats[chatID] = &core.Chat{ID: chatID, Title: title, Type: type_, Settings: settings}
+	return nil
+}
