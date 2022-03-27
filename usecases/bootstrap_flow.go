53af8927c613adfc08b502d16878ba9edcafac1e
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-03-27 16:31:50 +0300

    Add initial chat and user creation logic into `bootstrap_flow` and make it sync

---
+++ usecases/bootstrap_flow.go
@@ -0,0 +1,61 @@
+package usecases
+
+import (
+	"sync"
+
+	"github.com/ailinykh/pullanusbot/v2/core"
+)
+
+func CreateBootstrapFlow(l core.ILogger, chatStorage core.IChatStorage, userStorage core.IUserStorage) core.ITextHandler {
+	return &BootstrapFlow{l, chatStorage, userStorage, sync.Mutex{}}
+}
+
+type BootstrapFlow struct {
+	l           core.ILogger
+	chatStorage core.IChatStorage
+	userStorage core.IUserStorage
+	lock        sync.Mutex
+}
+
+// HandleText is a core.ITextHandler protocol implementation
+func (flow *BootstrapFlow) HandleText(message *core.Message, bot core.IBot) error {
+	flow.lock.Lock()
+	defer flow.lock.Unlock()
+
+	err := flow.ensureUserExists(message.Sender)
+	if err != nil {
+		flow.l.Error(err)
+		//Do not return?
+	}
+
+	err = flow.ensureChatExists(message.Chat)
+	if err != nil {
+		flow.l.Error(err)
+		//Do not return?
+	}
+
+	return err
+}
+
+func (flow *BootstrapFlow) ensureChatExists(chat *core.Chat) error {
+	_, err := flow.chatStorage.GetChatByID(chat.ID)
+	if err != nil {
+		if err.Error() == "record not found" {
+			settings := core.DefaultSettings()
+			return flow.chatStorage.CreateChat(chat.ID, chat.Title, chat.Type, &settings)
+		}
+		flow.l.Error(err)
+	}
+	return err
+}
+
+func (flow *BootstrapFlow) ensureUserExists(user *core.User) error {
+	_, err := flow.userStorage.GetUserById(user.ID)
+	if err != nil {
+		if err.Error() == "record not found" {
+			return flow.userStorage.CreateUser(user)
+		}
+		flow.l.Error(err)
+	}
+	return err
+}
---
+++ usecases/bootstrap_flow_test.go
@@ -0,0 +1,70 @@
+package usecases_test
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/ailinykh/pullanusbot/v2/test_helpers"
+	"github.com/ailinykh/pullanusbot/v2/usecases"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_HandleText_CreateUserData(t *testing.T) {
+	logger := test_helpers.CreateLogger()
+	chatStorage := test_helpers.CreateChatStorage()
+	userStorage := test_helpers.CreateUserStorage()
+	bootstrapFlow := usecases.CreateBootstrapFlow(logger, chatStorage, userStorage)
+
+	bot := test_helpers.CreateBot()
+
+	messages := []string{
+		"/start",
+		"/start payload",
+		"/start another_payload",
+	}
+	wg := sync.WaitGroup{}
+
+	for _, message := range messages {
+		wg.Add(1)
+		go func(text string) {
+			bootstrapFlow.HandleText(makeMessage(text), bot)
+			wg.Done()
+		}(message)
+	}
+
+	wg.Wait()
+
+	assert.Equal(t, 1, len(userStorage.Users))
+
+	message := makeMessage("/start")
+	user, _ := userStorage.GetUserById(message.Sender.ID)
+	assert.Equal(t, message.Sender, user)
+}
+
+func Test_HandleText_CreateChatData(t *testing.T) {
+	logger := test_helpers.CreateLogger()
+	chatStorage := test_helpers.CreateChatStorage()
+	userStorage := test_helpers.CreateUserStorage()
+	bootstrapFlow := usecases.CreateBootstrapFlow(logger, chatStorage, userStorage)
+
+	bot := test_helpers.CreateBot()
+
+	messages := []string{
+		"/start",
+		"/some_command",
+		"some text message",
+	}
+	wg := sync.WaitGroup{}
+
+	for _, message := range messages {
+		wg.Add(1)
+		go func(text string) {
+			bootstrapFlow.HandleText(makeMessage(text), bot)
+			wg.Done()
+		}(message)
+	}
+
+	wg.Wait()
+
+	assert.Equal(t, 1, len(chatStorage.Chats))
+}
