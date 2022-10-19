5140654d8acb0c856f74b2d1de7f5bc3c780921a
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-10-20 01:56:12 +0400

    Add download video factory for

---
+++ helpers/download_video_factory.go
@@ -0,0 +1,43 @@
+package helpers
+
+import (
+	"os"
+	"path"
+	"strings"
+
+	"github.com/ailinykh/pullanusbot/v2/core"
+)
+
+func CreateDownloadVideoFactory(l core.ILogger, fileDownloader core.IFileDownloader, videoFactory core.IVideoFactory) core.IVideoFactory {
+	return &DownloadVideoFactory{l, fileDownloader, videoFactory}
+}
+
+type DownloadVideoFactory struct {
+	l              core.ILogger
+	fileDownloader core.IFileDownloader
+	videoFactory   core.IVideoFactory
+}
+
+// CreateVideo is a core.IVideoFactory interface implementation
+func (factory *DownloadVideoFactory) CreateVideo(url string) (*core.Video, error) {
+	filename := path.Base(url)
+	if strings.Contains(filename, "?") {
+		parts := strings.Split(url, "?")
+		filename = path.Base(parts[0])
+	}
+
+	if !strings.HasSuffix(filename, ".mp4") {
+		filename = filename + ".mp4"
+	}
+
+	videoPath := path.Join(os.TempDir(), filename)
+	factory.l.Infof("downloading %s %s", videoPath, url)
+	file, err := factory.fileDownloader.Download(url, videoPath)
+	if err != nil {
+		factory.l.Error(err)
+		file.Dispose()
+		return nil, err
+	}
+	factory.l.Infof("file downloaded: %s %0.2fMB", file.Name, float64(file.Size)/1024/1024)
+	return factory.videoFactory.CreateVideo(file.Path)
+}
