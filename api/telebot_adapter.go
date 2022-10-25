19a7e6263ef9b85766fb4a79613daf90cb1791a4
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-10-25 21:41:58 +0400

    More logs in TelebotAdapter SendVideo func

--- api/telebot_adapter.go
+++ api/telebot_adapter.go
@@ -157,7 +157,7 @@ func (a *TelebotAdapter) SendPhotoAlbum(medias []*core.Media) ([]*core.Message, 
 
 // SendVideo is a core.IBot interface implementation
 func (a *TelebotAdapter) SendVideo(vf *core.Video, caption string) (*core.Message, error) {
-	a.t.logger.Infof("uploading video %s", vf.Name)
+	a.t.logger.Infof("uploading video %s (%.2f MB)", vf.Name, float64(vf.Size)/2014/2014)
 	video := makeTbVideo(vf, caption)
 	a.t.bot.Notify(a.m.Chat, tb.UploadingVideo)
 	sent, err := video.Send(a.t.bot, a.m.Chat, &tb.SendOptions{ParseMode: tb.ModeHTML})
