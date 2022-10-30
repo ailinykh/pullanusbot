685d35f081efbea71735a9dd05c7465098d5fe4f
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-10-30 14:57:25 +0400

    Add `IBoolSettingProvider` interface which represent a boolean setting

--- core/settings_provider.go
+++ core/settings_provider.go
@@ -4,3 +4,8 @@ type ISettingsProvider interface {
 	GetData(ChatID, SettingKey) ([]byte, error)
 	SetData(ChatID, SettingKey, []byte) error
 }
+
+type IBoolSettingProvider interface {
+	GetBool(ChatID, SettingKey) bool
+	SetBool(ChatID, SettingKey, bool) error
+}
