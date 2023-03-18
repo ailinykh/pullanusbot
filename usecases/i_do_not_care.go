a59f55d96320d7d0faf481f319d0523a18359fd7
Anthony Ilinykh <anthonyilinykh@gmail.com>
2023-03-18 19:04:11 +0400

    Fix for `i do not care` flow

--- usecases/i_do_not_care.go
+++ usecases/i_do_not_care.go
@@ -15,7 +15,14 @@ type IDoNotCare struct{}
 // HandleText is a core.ITextHandler protocol implementation
 func (IDoNotCare) HandleText(message *core.Message, bot core.IBot) error {
 	if strings.Contains(strings.ToLower(message.Text), "мне всё равно") {
-		_, err := bot.SendVideo(&core.Video{ID: "BAACAgIAAxkBAAIfmmNP8h0AAbIFnYs3RhAVnHQQxq3DjgAC9CAAAkxOgUriVsO_AcngZSoE"}, "")
+		_, err := bot.SendVideo(&core.Video{ID: "BAACAgIAAxkBAAEDfu1kFdKdAAHM4iO92LOC3muxi2yyvosAAgQoAAIZvLFIVaKgRXqfmVgvBA"}, "")
+		if err != nil {
+			media := &core.Media{
+				ResourceURL: "https://telegra.ph/file/182c624365bea4df6842a.mp4",
+				Type:        core.TVideo,
+			}
+			_, err = bot.SendMedia(media)
+		}
 		return err
 	}
 	if strings.Contains(strings.ToLower(message.Text), "привет, андрей") {
