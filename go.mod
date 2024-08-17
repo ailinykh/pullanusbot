5eadef91a20d5e37e66bc74076f893898a21a4f1
Anthony Ilinykh <anthonyilinykh@gmail.com>
2024-08-17 08:24:28 +0300

    Migrate `LightsailAPI` to `core.Logger` interface

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ailinykh/pullanusbot/v2
 go 1.22
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.30.3
 	github.com/aws/aws-sdk-go-v2/service/lightsail v1.40.3
 	github.com/google/logger v1.1.1
 	github.com/google/uuid v1.6.0
@@ -14,7 +15,6 @@ require (
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.30.3 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.15 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.15 // indirect
 	github.com/aws/smithy-go v1.20.3 // indirect
--- internal/legacy/api/lightsail_api.go
+++ internal/legacy/api/lightsail_api.go
@@ -2,13 +2,15 @@ package api
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/ailinykh/pullanusbot/v2/internal/legacy/core"
+	"github.com/ailinykh/pullanusbot/v2/internal/core"
+	legacy "github.com/ailinykh/pullanusbot/v2/internal/legacy/core"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/lightsail"
 )
 
-func NewLightsailAPI(logger core.ILogger, accessKeyId, secretAccessKey string) core.ServerAPI {
+func NewLightsailAPI(logger core.Logger, accessKeyId, secretAccessKey string) legacy.ServerAPI {
 	client := lightsail.New(lightsail.Options{
 		Region: "eu-central-1",
 		Credentials: aws.CredentialsProviderFunc(func(ctx context.Context) (aws.Credentials, error) {
@@ -26,19 +28,19 @@ func NewLightsailAPI(logger core.ILogger, accessKeyId, secretAccessKey string) c
 
 type LightsailAPI struct {
 	client *lightsail.Client
-	logger core.ILogger
+	logger core.Logger
 }
 
-func (api *LightsailAPI) GetServers(ctx context.Context) ([]*core.Server, error) {
+func (api *LightsailAPI) GetServers(ctx context.Context) ([]*legacy.Server, error) {
 	output, err := api.client.GetInstances(ctx, &lightsail.GetInstancesInput{})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("get instances request failed: %w", err)
 	}
 
-	servers := []*core.Server{}
+	servers := []*legacy.Server{}
 	for _, instance := range output.Instances {
 		if instance.Name != nil {
-			server := core.Server{
+			server := legacy.Server{
 				Name: *instance.Name,
 			}
 			servers = append(servers, &server)
@@ -47,10 +49,10 @@ func (api *LightsailAPI) GetServers(ctx context.Context) ([]*core.Server, error)
 	return servers, nil
 }
 
-func (api *LightsailAPI) RebootServer(ctx context.Context, server *core.Server) error {
+func (api *LightsailAPI) RebootServer(ctx context.Context, server *legacy.Server) error {
 	output, err := api.client.RebootInstance(ctx, &lightsail.RebootInstanceInput{
 		InstanceName: &server.Name,
 	})
-	api.logger.Infof("reboot server result %+v", output)
+	api.logger.Info("reboot server result", "output", output)
 	return err
 }
