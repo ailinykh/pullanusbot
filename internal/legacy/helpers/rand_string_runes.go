86dc7d0196b79c0a404087a1c840e238ac70a58d
Anthony Ilinykh <anthonyilinykh@gmail.com>
2024-10-27 20:30:55 +0300

    As of Go 1.20 there is no reason to call Seed with a random value

--- internal/legacy/helpers/rand_string_runes.go
+++ internal/legacy/helpers/rand_string_runes.go
@@ -2,14 +2,12 @@ package helpers
 
 import (
 	"math/rand"
-	"time"
 )
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 // RandStringRunes returns a random n-length string
 func RandStringRunes(n int) string {
-	rand.Seed(time.Now().UTC().UnixNano())
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
--- internal/legacy/infrastructure/math_rand.go
+++ internal/legacy/infrastructure/math_rand.go
@@ -2,13 +2,11 @@ package infrastructure
 
 import (
 	"math/rand"
-	"time"
 
 	"github.com/ailinykh/pullanusbot/v2/internal/legacy/core"
 )
 
 func CreateMathRand() core.IRand {
-	rand.Seed(time.Now().UTC().UnixNano())
 	return MathRand{}
 }
 
